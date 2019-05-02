370b85189a76acb0bc80c4fa3bc8f08947aa1e34
enjesther <49730425+enjesther@users.noreply.github.com>
2019-05-02 13:24:15 -0300

    Add files via upload

---
+++ Desafio2/features/Desafio.feature
@@ -0,0 +1,7 @@
+#language: pt
+
+Funcionalidade:  Filtrar busca no site americanas
+    Cenario: Entrar na sessao Dia das Maes e entrar na pagina de algum produto
+        Dado que estou na pagina principal 
+        Quando seleciono o tipo Moda
+        Entao ele deve filtrar os produtos do menor para o maior
---
+++ Desafio2/steps/novo.py
@@ -0,0 +1,15 @@
+import time
+
+@given(u'que estou na pagina principal')
+def step_impl(context):
+    context.home.go_home()
+
+
+@when(u'seleciono o tipo Moda')
+def step_impl(context):
+    context.home.selecionar_moda()
+
+
+@then(u'ele deve filtrar os produtos do menor para o maior')
+def step_impl(context):
+    context.home.filtrar()
